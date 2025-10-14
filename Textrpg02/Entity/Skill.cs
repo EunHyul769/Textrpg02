b3c64a7431df1aa8bf05a14893b375060e26a653
Jihun <gowvw77@naver.com>
2025-10-14 20:27:25 +0900

    스킬cs 추가/변경

---
+++ Textrpg02/Entity/Skill.cs
@@ -0,0 +1,32 @@
+﻿
+namespace TextRPG.Entity
+{
+    internal class Skill
+    {
+        public string Name { get; set; }
+        public double Power { get; set; }  // 물딜 배율
+        public double SPower { get; set; }  //마딜 배율
+
+        public int MpCost { get; set; }
+
+        public Skill(string name, double power, double spower, int mpCost)
+        {
+            Name = name;
+            Power = power;
+            SPower = spower;
+            MpCost = mpCost;
+        }
+
+        // atk*배율 + spd*배율 + luk*배율 + def*배율 + 고정치?
+        public double CalculateAttackDamage(Player player)
+        {
+            return player.Attack * Power;
+        }
+
+        public double CalculateSkillAttackDamage(Player player)
+        {
+            return player.SkillAttack * SPower;
+        }
+    }
+
+}
