790e366dacfb2a61ad5ebbaf4366ad0d41b28883
Jihun <gowvw77@naver.com>
2025-10-15 11:14:38 +0900

    feat:스킬계수 고정치추가

--- Textrpg02/Entity/Skill.cs
+++ Textrpg02/Entity/Skill.cs
@@ -7,13 +7,16 @@ namespace TextRPG.Entity
         public double Power { get; set; }  // 물딜 배율
         public double SPower { get; set; }  //마딜 배율
 
+        public int StaticValue { get; set; } //계수 없는 고정 수치
+
         public int MpCost { get; set; }
 
-        public Skill(string name, double power, double spower, int mpCost)
+        public Skill(string name, double power, double spower, int sv, int mpCost)
         {
             Name = name;
             Power = power;
             SPower = spower;
+            StaticValue = sv;
             MpCost = mpCost;
         }
 
@@ -22,8 +25,9 @@ namespace TextRPG.Entity
         {
             double addamage = character.Attack * Power;    //물딜 따로 계산.
             double apdamage = character.SkillAttack * SPower;  //마딜 따로 계산
+            int staticdamage = StaticValue;                 //고정 데미지는 스킬 표기값 그대로 적용
 
-            double totaldamage = addamage + apdamage;   //총 딜로 합산
+            double totaldamage = addamage + apdamage + staticdamage;   //총 딜로 합산
 
             return Math.Round(totaldamage, 0);  //소수 전부 반올림
         }
