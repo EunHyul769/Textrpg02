1da5f0e1b4fd7dcdb8fcab80484615aec739132f
qkrtkdgus010208 <psh010208@naver.com>
2025-10-15 14:51:49 +0900

    refactor: 아이템 DB에 맞게 수정

--- Textrpg02/Data/DB/ConsumeItemDB.cs
+++ Textrpg02/Data/DB/ConsumeItemDB.cs
@@ -6,7 +6,9 @@ namespace TextRPG.Data.DB
     {
         public Dictionary<int, ConsumeItem> Items { get; private set; } = new Dictionary<int, ConsumeItem>()
         {
-            { 1, new ConsumeItem("소비 아이템", "체력을 50 회복", 1000, 50, 0) },
+            { 1000, new ConsumeItem("빨간 포션", "Hp를 50 회복시켜줍니다.", 1000, 50, 0) },
+            { 1001, new ConsumeItem("파란 포션", "Mp를 50 회복시켜줍니다.", 1000, 0, 50) },
+            { 1002, new ConsumeItem("엘릭서", "Hp, Mp를 모두 50 회복시켜줍니다.", 2000, 50, 50) },
         };
 
         public ConsumeItem? GetByKey(int key)
--- Textrpg02/Data/DB/EquipItemDB.cs
+++ Textrpg02/Data/DB/EquipItemDB.cs
@@ -6,7 +6,16 @@ namespace TextRPG.Data.DB
     {
         public Dictionary<int, EquipItem> Items { get; private set; } = new Dictionary<int, EquipItem>()
         {
-            { 1, new EquipItem("전사 무기1", "전사 무기1...", 1000, Enum.ItemType.Equip, Enum.JobType.Warrior, Enum.EquipSlot.Weapon, 0, 0, 10, 5, 0) },
+            { 2000, new EquipItem("연습용 칼", "연습할 때 쓰던 칼", 1000, Enum.ItemType.Equip, Enum.JobType.Warrior, Enum.EquipSlot.Weapon, 0, 0, 10, 5, 0) },
+            { 2001, new EquipItem("연습용 활", "연습할 때 쓰던 활", 1000, Enum.ItemType.Equip, Enum.JobType.Archer, Enum.EquipSlot.Weapon, 0, 0, 10, 5, 0) },
+            { 2002, new EquipItem("연습용 지팡이", "연습할 때 쓰던 지팡이", 1000, Enum.ItemType.Equip, Enum.JobType.Mage, Enum.EquipSlot.Weapon, 0, 0, 10, 5, 0) },
+
+            { 3000, new EquipItem("일반 판금 갑옷", "철로 만든 갑옷", 1000, Enum.ItemType.Equip, Enum.JobType.Warrior, Enum.EquipSlot.Armor, 50, 10, 0, 0, 5, 5) },
+            { 3001, new EquipItem("고급 판금 갑옷", "강철로 만든 갑옷", 1000, Enum.ItemType.Equip, Enum.JobType.Warrior, Enum.EquipSlot.Armor, 100, 20, 0, 0, 10, 10) },
+            { 3002, new EquipItem("일반 가죽 갑옷", "가죽으로 만든 갑옷", 1000, Enum.ItemType.Equip, Enum.JobType.Archer, Enum.EquipSlot.Armor, 50, 10, 0, 0, 5, 5) },
+            { 3003, new EquipItem("고급 가죽 갑옷", "고급 가죽으로 만든 갑옷", 1000, Enum.ItemType.Equip, Enum.JobType.Archer, Enum.EquipSlot.Armor, 100, 20, 0, 0, 10, 10) },
+            { 3004, new EquipItem("일반 천 갑옷", "천으로 만든 갑옷", 1000, Enum.ItemType.Equip, Enum.JobType.Mage, Enum.EquipSlot.Armor, 50, 10, 0, 0, 5, 5) },
+            { 3005, new EquipItem("고급 천 갑옷", "고급 천으로 만든 갑옷", 1000, Enum.ItemType.Equip, Enum.JobType.Mage, Enum.EquipSlot.Armor, 100, 20, 0, 0, 10, 10) },
         };
 
         public EquipItem? GetByKey(int key)
